7fba590a085d17d884ded49294545f373f4b9306
marcellosso <marcel.losso@gmail.com>
2022-10-14 20:11:30 -0300

    Logging on cors

--- src/cors.ts
+++ src/cors.ts
@@ -3,6 +3,7 @@ import { ALLOWED_ORIGINS, NODE_ENV } from "./config"
 // TODO Remove any?
 export const corsAllOptions = {
   origin: (origin: any, callback: any) => {
+    console.log('Incoming Origin - ', origin);
     if (ALLOWED_ORIGINS.indexOf(origin) !== -1 || (!origin && NODE_ENV === 'local')) {
       callback(null, true);
     } else {
