c9fd2196b1c7f8a0d13b77899d6c2820065c8186
marcellosso <marcel.losso@gmail.com>
2023-03-06 09:05:42 -0300

    updated s3 file to only import s3 from aws-sdk

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -1,12 +1,8 @@
-import aws from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3';
 import { AWS_SECRET_KEY, AWS_ACCESS_KEY, AWS_REGION } from '../config';
 
-aws.config.update({
+export default new S3({
   secretAccessKey: AWS_SECRET_KEY,
   accessKeyId: AWS_ACCESS_KEY,
   region: AWS_REGION
-});
-
-const S3 = new aws.S3();
-
-export default S3;
+});;
