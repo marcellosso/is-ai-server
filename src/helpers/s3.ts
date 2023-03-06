c8c994afa8ae03f75144074a05097a86103a7027
marcellosso <marcel.losso@gmail.com>
2023-03-06 13:26:04 -0300

    row back s3 implementation

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -1,8 +1,8 @@
-import S3 from 'aws-sdk/clients/s3';
-import { AWS_SECRET_KEY, AWS_ACCESS_KEY, AWS_REGION } from '../config';
+// import S3 from 'aws-sdk/clients/s3';
+// import { AWS_SECRET_KEY, AWS_ACCESS_KEY, AWS_REGION } from '../config';
 
-export default new S3({
-  secretAccessKey: AWS_SECRET_KEY,
-  accessKeyId: AWS_ACCESS_KEY,
-  region: AWS_REGION
-});;
+// export default new S3({
+//   secretAccessKey: AWS_SECRET_KEY,
+//   accessKeyId: AWS_ACCESS_KEY,
+//   region: AWS_REGION
+// });;
--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -1,23 +1,36 @@
 import multer from "multer";
-import multerS3 from 'multer-s3';
-import { AWS_BUCKET_NAME } from "../config";
-import S3 from "../helpers/s3";
+// import multerS3 from 'multer-s3';
+// import { AWS_BUCKET_NAME } from "../config";
+// import S3 from "../helpers/s3";
 
 const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'src/public/img')
+  },
+  filename: (req, file, cb) => {
+    // const splittedName = file.originalname.split('.');
+    // const fileExtension = splittedName[splittedName.length - 1];
+    // cb(null, `${file.filename}.${fileExtension}`);
+    cb(null, file.originalname);
+  }
+})
 
 const upload = multer({
   limits: {
     fileSize: 4 * 1024 * 1024,
   },
-  storage: multerS3({
-    s3: S3 as any,
-    bucket: AWS_BUCKET_NAME,
-    acl: 'public-read',
-    cacheControl: 'max-age=31536000',
-    key: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`); //use Date.now() for unique file keys
-    }
-  }),
+  storage,
+  // storage: multerS3({
+  //   s3: S3 as any,
+  //   bucket: AWS_BUCKET_NAME,
+  //   acl: 'public-read',
+  //   cacheControl: 'max-age=31536000',
+  //   key: function (req, file, cb) {
+  //       cb(null, `${Date.now()}-${file.originalname}`); //use Date.now() for unique file keys
+  //   }
+  // }),
   fileFilter: (req, file, cb) => {
     if (ALLOWED_IMAGE_TYPES.includes(file.mimetype)) {
       cb(null, true);
